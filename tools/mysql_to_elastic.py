4196132fc04dafec909acb66180181425431448f
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-10-01 13:17:08 +0200

    Added a way to index ALL the articles in the DB

--- tools/mysql_to_elastic.py
+++ tools/mysql_to_elastic.py
@@ -17,9 +17,11 @@ from zeeguu.core.model.article import article_topic_map
 from zeeguu.core.elastic.settings import ES_ZINDEX, ES_CONN_STRING
 import numpy as np
 
+# PARAMS That can be Adjusted for the script.
+
 DELETE_INDEX = False
-TOTAL_ITEMS = 40000
-ITERATION_STEP = 100
+TOTAL_ITEMS = "ALL"
+ITERATION_STEP = 1000
 
 app = create_app()
 app.app_context().push()
@@ -71,13 +73,9 @@ def main():
                 print(f"fail for: '{article.id}', {e}")
 
     # Get All the IDs
-    all_article_ids = np.array(
-        [
-            a_id[0]
-            for a_id in db_session.query(Article.id)
-            .all()
-        ]
-    )
+    # You can alter the Query, to for example only index the DB articles for a language.
+    # e.g. db_session.query(Article.id).filter(Article.language_id == 2).all() for DK only.
+    all_article_ids = np.array([a_id[0] for a_id in db_session.query(Article.id).all()])
     # Filter can be used to retrieve specific language.
     print(f"Total Articles in DB: {len(all_article_ids)}")
     # Filter out those not in ES
@@ -88,18 +86,24 @@ def main():
     print("Total articles missing: ", len(all_article_ids_not_in_es))
     # I noticed that if a document is not added then it won't let me query the ES search.
     total_added = 0
+
+    # Total Items to Handle
+    total_items = (
+        len(all_article_ids_not_in_es) if TOTAL_ITEMS == "ALL" else TOTAL_ITEMS
+    )
+
     sampled_ids = np.random.choice(
         all_article_ids_not_in_es,
-        min(TOTAL_ITEMS, len(all_article_ids_not_in_es)),
+        min(total_items, len(all_article_ids_not_in_es)),
         replace=False,
     )
-    for i_start in range(0, TOTAL_ITEMS, ITERATION_STEP):
+    for i_start in range(0, total_items, ITERATION_STEP):
         print(f"Starting at {i_start}")
         sub_sample = sampled_ids[i_start : i_start + ITERATION_STEP]
         try:
             res, _ = bulk(es, gen_docs(fetch_articles_by_id(sub_sample)))
             total_added += res
-            print(f"Completed {i_start+ITERATION_STEP}/{TOTAL_ITEMS}...")
+            print(f"Completed {i_start+ITERATION_STEP}/{total_items}...")
         except BulkIndexError:
             print("-- WARNING, at least one document failed to index.")
     print(f"Total articles added: {total_added}")
